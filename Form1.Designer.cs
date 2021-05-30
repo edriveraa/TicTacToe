6bd1f7b28d2305dc26c9b0899ba2208cfb68272a
edriveraa <67114693+edriveraa@users.noreply.github.com>
2021-05-30 13:49:35 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Tab1 = new System.Windows.Forms.Button();
+            this.Tab2 = new System.Windows.Forms.Button();
+            this.Tab3 = new System.Windows.Forms.Button();
+            this.Tab4 = new System.Windows.Forms.Button();
+            this.Tab5 = new System.Windows.Forms.Button();
+            this.Tab6 = new System.Windows.Forms.Button();
+            this.Tab7 = new System.Windows.Forms.Button();
+            this.Tab8 = new System.Windows.Forms.Button();
+            this.Tab9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tab1
+            // 
+            this.Tab1.Location = new System.Drawing.Point(12, 49);
+            this.Tab1.Name = "Tab1";
+            this.Tab1.Size = new System.Drawing.Size(88, 80);
+            this.Tab1.TabIndex = 0;
+            this.Tab1.UseVisualStyleBackColor = true;
+            // 
+            // Tab2
+            // 
+            this.Tab2.Location = new System.Drawing.Point(106, 49);
+            this.Tab2.Name = "Tab2";
+            this.Tab2.Size = new System.Drawing.Size(88, 80);
+            this.Tab2.TabIndex = 1;
+            this.Tab2.UseVisualStyleBackColor = true;
+            // 
+            // Tab3
+            // 
+            this.Tab3.Location = new System.Drawing.Point(200, 49);
+            this.Tab3.Name = "Tab3";
+            this.Tab3.Size = new System.Drawing.Size(87, 80);
+            this.Tab3.TabIndex = 2;
+            this.Tab3.UseVisualStyleBackColor = true;
+            // 
+            // Tab4
+            // 
+            this.Tab4.Location = new System.Drawing.Point(12, 135);
+            this.Tab4.Name = "Tab4";
+            this.Tab4.Size = new System.Drawing.Size(88, 80);
+            this.Tab4.TabIndex = 3;
+            this.Tab4.UseVisualStyleBackColor = true;
+            // 
+            // Tab5
+            // 
+            this.Tab5.Location = new System.Drawing.Point(106, 135);
+            this.Tab5.Name = "Tab5";
+            this.Tab5.Size = new System.Drawing.Size(88, 80);
+            this.Tab5.TabIndex = 4;
+            this.Tab5.UseVisualStyleBackColor = true;
+            // 
+            // Tab6
+            // 
+            this.Tab6.Location = new System.Drawing.Point(199, 135);
+            this.Tab6.Name = "Tab6";
+            this.Tab6.Size = new System.Drawing.Size(88, 80);
+            this.Tab6.TabIndex = 5;
+            this.Tab6.UseVisualStyleBackColor = true;
+            // 
+            // Tab7
+            // 
+            this.Tab7.Location = new System.Drawing.Point(12, 221);
+            this.Tab7.Name = "Tab7";
+            this.Tab7.Size = new System.Drawing.Size(88, 80);
+            this.Tab7.TabIndex = 6;
+            this.Tab7.UseVisualStyleBackColor = true;
+            // 
+            // Tab8
+            // 
+            this.Tab8.Location = new System.Drawing.Point(106, 221);
+            this.Tab8.Name = "Tab8";
+            this.Tab8.Size = new System.Drawing.Size(88, 80);
+            this.Tab8.TabIndex = 7;
+            this.Tab8.UseVisualStyleBackColor = true;
+            // 
+            // Tab9
+            // 
+            this.Tab9.Location = new System.Drawing.Point(199, 221);
+            this.Tab9.Name = "Tab9";
+            this.Tab9.Size = new System.Drawing.Size(88, 80);
+            this.Tab9.TabIndex = 8;
+            this.Tab9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 320);
+            this.Controls.Add(this.Tab9);
+            this.Controls.Add(this.Tab8);
+            this.Controls.Add(this.Tab7);
+            this.Controls.Add(this.Tab6);
+            this.Controls.Add(this.Tab5);
+            this.Controls.Add(this.Tab4);
+            this.Controls.Add(this.Tab3);
+            this.Controls.Add(this.Tab2);
+            this.Controls.Add(this.Tab1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Tab1;
+        private System.Windows.Forms.Button Tab2;
+        private System.Windows.Forms.Button Tab3;
+        private System.Windows.Forms.Button Tab4;
+        private System.Windows.Forms.Button Tab5;
+        private System.Windows.Forms.Button Tab6;
+        private System.Windows.Forms.Button Tab7;
+        private System.Windows.Forms.Button Tab8;
+        private System.Windows.Forms.Button Tab9;
+    }
+}
+
